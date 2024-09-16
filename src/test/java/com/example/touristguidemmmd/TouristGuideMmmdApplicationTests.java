c73afc8cb9068fdba4f35c54079ae450db17091f
MetteMunch <meha0005@stud.kea.dk>
2024-09-16 10:28:52 +0200

    Lavet diverse packages

---
+++ src/test/java/com/example/touristguidemmmd/TouristGuideMmmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.touristguidemmmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristGuideMmmdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
